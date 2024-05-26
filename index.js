9cb34e697fb99f17f475740b10a2ad5b96ac45a0
Ann <a.m.hnatyshyn@gmail.com>
2024-05-26 12:52:38 -0300

    add fetchflyovertimes to index file

--- index.js
+++ index.js
@@ -17,3 +17,16 @@ fetchCoordsByIP('24.222.102.18', (error, coordinates) => {
   }
   console.log('It worked! Returned coordinates:' , coordinates);
 });
+
+const { fetchISSFlyOverTimes } = require('./iss');
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+});
