f0ef1d541cb9c60cb5f41d4aac9acf7faaaec71b
Ann <a.m.hnatyshyn@gmail.com>
2024-05-26 11:29:09 -0300

    add fetchCoordsBy IP function

--- iss.js
+++ iss.js
@@ -1,16 +1,4 @@
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
-// inside the request callback ...
-// error can be set if invalid domain, user is offline, etc.
-
 const needle = require('needle');
-
 
 const fetchMyIP = function(callback) {
   
@@ -26,8 +14,23 @@ const fetchMyIP = function(callback) {
     const bodyObj = JSON.parse(body);
     const ip = bodyObj.ip;
     callback(null, ip);
-    // console.log(body.ip_addr);
   });
 };
 
+const fetchCoordsByIP = function(ip, callback) {
+  needle.get("http://ipwho.is/?lang=en", function(error, response, body) {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const bodyObj = JSON.parse(body);
+    const ip = bodyObj.ip;
+    callback(null, ip);
+  });
+};
+
+
+
+
+module.exports = { fetchCoordsByIP };
 module.exports = { fetchMyIP };
