231d67937bddb87dd62b8714bfec28dcb7072701
Ann <a.m.hnatyshyn@gmail.com>
2024-05-26 15:51:51 -0300

    complete checking with compass

--- index2.js
+++ index2.js
@@ -1,6 +1,14 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss_promised.js');
+// const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss_promised.js');
+const {nextISSTimesForMyLocation} = require('./iss_promised.js');
 
-
-fetchMyIP()
-  .then((ip) => fetchCoordsByIP(ip))
-  .then((body) => console.log(body));
+// fetchMyIP()
+//   .then((ip) => fetchCoordsByIP(ip))
+//   .then((coords) => fetchISSFlyOverTimes(coords))
+//   .then((body) => console.log(body));
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    console.log(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
--- iss_promised.js
+++ iss_promised.js
@@ -1,32 +1,41 @@
-const needle = require('needle');
+const needle = require("needle");
 
-const fetchMyIP = function(callback) {
-  return needle('get', 'https://api.ipify.org?format=json')
-    .then((response) => {
-      const body = response.body; // retrieve the body value from the response object
-      const ip = body.ip; // retrieve the ip from the body object
-      return ip;
+const fetchMyIP = function (callback) {
+  return needle("get", "https://api.ipify.org?format=json").then((response) => {
+    const body = response.body; // retrieve the body value from the response object
+    const ip = body.ip; // retrieve the ip from the body object
+    return ip;
+  });
+};
+
+const fetchCoordsByIP = function (ip) {
+  return needle("get", "`http://ipwho.is/${ip}`").then((response) => {
+    const body = response.body;
+    const latitude = body.latitude;
+    const longitude = body.longitude;
+    return { latitude, longitude };
+  });
+};
+
+const fetchISSFlyOverTimes = function (coords) {
+  const latitude = coords.latitude;
+  const longitude = coords.longitude;
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
+  return needle("get", url).then((response) => {
+    const body = response.body;
+    const passtimes = body.response; // changed the name from response to passtimes for clarification
+    return passtimes;
+  });
+};
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then((ip) => fetchCoordsByIP(ip))
+    .then((coords) => fetchISSFlyOverTimes(coords))
+    .then((passtimes) => {
+      return passtimes;
     });
 };
 
-const fetchCoordsByIP = function(ip) {
-  return needle('get', '`http://ipwho.is/${ip}`')
-    .then((response) => {
-      const body = response.body;
-      const latitude = body.latitude;
-      const longitude = body.longitude;
-      return {latitude, longitude};
-    });
-};
-
-const fetchISSFlyOverTimes = function() {
-  return needle('get', '` https://iss-flyover.herokuapp.com `')
-    .then((response) => {
-      const body = response.body;
-      const flyOverTimes = body.flyOverTimes;
-      return flyOverTimes;
-    });
-};
-
-
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+module.exports = { nextISSTimesForMyLocation };
+// module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
